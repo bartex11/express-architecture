b1b4c24ec23df512185ae4ea1d4f2a118a995e5b
bartonization <i_vasilev@ymail.com>
2016-12-01 12:13:46 +0100

    Express and Mongo DB running !!!

---
+++ index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const express = require('express')
+
+let app = express()
+
+app.get('/',(req, res) =>{
+    mongoose.connect('mongodb://localhost:27017/some-express-db')
+    
+    console.log('MongoDb is ready')
+
+    res.send('Coolll!!')
+
+})
+
+app.listen(1234)
+console.log('Express is ready!')
